67486030b61711228db03f32ab8a89ef03feb708
Your Name <dat054760@gmail.com>
2022-07-24 22:24:39 +0700

    up

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height");
+        height = scanner.nextFloat();
+        float area = width*height;
+        System.out.println("Area is: " +area);
+    }
+}
